006ca15257637563e2b010e4024efca5891698a2
hasanmainul <mainul.haque@framgia.com>
2015-10-01 16:01:10 +0700

    Question add with nested Answers Attribute

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,5 +5,11 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
 
   validates :subject_id, presence: true
-  validates :content, presence: true, length: {maximum: 80}
+  validates :content, presence: true, length: {maximum: 80},
+    uniqueness: true
+  validates_associated :answers
+
+  accepts_nested_attributes_for :answers,
+    reject_if: proc {|attributes| attributes["content"].blank?},
+    allow_destroy: true
 end
