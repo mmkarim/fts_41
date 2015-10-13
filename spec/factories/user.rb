866f76eb96b5ac1a0eef1ff78290057fb85ac729
mmkarim <md.muzahidul.karim@framgia.com>
2015-10-13 13:40:25 +0700

    Try Rspec

---
+++ spec/factories/user.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+  factory :user do
+    email {Faker::Internet.email}
+    admin false
+    password "123123123"
+
+    factory :admin do
+      admin true
+    end
+
+    factory :invalid_password do
+      password ""
+    end
+
+    factory :invalid_email do
+      email ""
+    end
+
+    factory :another_user do
+      name {Faker::Name.name}
+      email {Faker::Internet.email}
+    end
+  end
+end
